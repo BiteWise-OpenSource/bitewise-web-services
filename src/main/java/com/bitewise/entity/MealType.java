f9aaa95a3bb751f8bfa570d153a649d1288ce3f6
malumunayco <malumunaycoa05@gmail.com>
2026-07-09 23:20:27 -0500

    chore: project reset

--- src/main/java/com/bitewise/entity/MealType.java
+++
@@ -1,8 +0,0 @@
-package com.bitewise.entity;
-
-public enum MealType {
-    BREAKFAST,
-    LUNCH,
-    DINNER,
-    SNACK
-}
--- src/main/java/com/bitewise/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitewise.repository;
-
-import com.bitewise.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-}
